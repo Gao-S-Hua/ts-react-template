93893461ff5d41202ae97a88d472f2f85816c8ec
Shaohua Gao <shaohuag@xlnx.xilinx.com>
2020-09-02 18:33:10 +0800

    r1

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
